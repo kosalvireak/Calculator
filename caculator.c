74d444a28bd23b182fecc7102cc9223b0100b8b3
khoeunkosalvireak <virakvary@gmail.com>
2022-05-13 21:58:48 +0700

    add + and -

--- caculator.c
+++ caculator.c
@@ -1,10 +1,35 @@
 #include<stdio.h>
 #include<math.h>
 int first_num, sec_num;
-main()
+char num;
+int main()
 {
-  printf("Enter your math operation: (+,-,*,/,^");
+  printf("Enter your math operation: (+,-,*,/,^) ");
   num = getchar();
     switch(num)
+    {
+      case '+':
+        printf("You can do addition.\n");
+        printf("Enter your first number: ");
+        scanf("%d",&first_num);
+        printf("Enter your seconde number: ");
+        scanf("%d",&sec_num);
+        printf("%d + %d = %d",first_num,sec_num,first_num+sec_num);
+        break;
+      case '-':
+        printf("You can do subtraction.\n");
+        printf("Enter your first number: ");
+        scanf("%d",&first_num);
+        printf("Enter your seconde number: ");
+        scanf("%d",&sec_num);
+        printf("%d - %d = %d",first_num,sec_num,first_num-sec_num);
+        break;
+
+
+
+
+    }
+      
+
   return 0;
 }
